31a821cc398375ec0cfadeeb44f81cc0988dc5e9
azrapatvi <azrapatvi@gmail.com>
2025-07-23 15:45:37 +0530

    Delete text_to_audio.py

--- text_to_audio.py
+++
@@ -1,8 +0,0 @@
-from gtts import gTTS  # or any other TTS lib you use
-import os
-
-def text_to_speech_file(text, folder):
-    tts = gTTS(text)
-    output_path = f"8_vidsnapAI_project/user_uploads/{folder}/audio.mp3"
-    tts.save(output_path)
-    print(f"✅ Audio saved at: {output_path}")
